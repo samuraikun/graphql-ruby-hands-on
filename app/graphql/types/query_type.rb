78e89a5c4c997dd0b520322f8f98233bc29fcb5b
samuraikun <tazitaziawawa@gmail.com>
2023-01-08 05:52:28 +0000

    impl User's GraphQL Query & Mutations

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,15 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :user, Types::UserType, null: true do
+      description "Find User by ID"
+      argument :id, ID, required: true
+    end
+    def user(id:)
+      User.find(id)
+    end
+    def users(page: nil, items: nil)
+      User.all
     end
   end
 end
