2e19e6b1c143b481c001d1c09bca4d94b58f7871
samuraikun <tazitaziawawa@gmail.com>
2023-01-08 04:06:06 +0000

    add User

---
+++ db/migrate/20230108035416_create_users.rb
@@ -0,0 +1,19 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :email, null: false, unique: true
+      t.string :password_digest, null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :username, null: false, unique: true
+      t.string :postal_code
+      t.string :address
+      t.string :phone
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+    add_index :users, :username, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_08_035416) do
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "username", null: false
+    t.string "postal_code"
+    t.string "address"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
 end
